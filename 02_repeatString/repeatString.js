269ee86540b1af207887cd4fe22d2ce5f9d1c4f7
bodiebice <bodiebice@gmail.com>
2023-08-30 11:40:45 -0500

    Completed the second tests with all tests passing

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, nums = 3) {
+  let string = "";
+  if (nums < 0){
+    return "ERROR";
+  }
+  for (x =0; x<nums; x++){
+    string = string.concat(word);
+  }
+  return string;
 };
 
 // Do not edit below this line
