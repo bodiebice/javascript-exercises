29e3cde1ae0a49118fde015c0c3e0b0e4ece7cef
bodiebice <bodiebice@gmail.com>
2023-08-31 22:30:32 -0500

    soln for 05, runtime 14% faster than expected

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(beg, end) {
+  if (beg < 0 || end < 0  || typeof(beg)!= "number" || typeof(end) != "number"){
+    return 'ERROR';
+  }
+  let sum = 0;
+  if (end < beg){
+    let temp = beg;
+    beg = end;
+    end = temp;
+  }
+  for (x = beg; x<end; x++ ){
+    sum += x;
+  }
+  sum += end;
+  return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
