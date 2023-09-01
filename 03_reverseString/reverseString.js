dd6e8e5655b52ee5101f00d006f0980603fbd823
bodiebice <bodiebice@gmail.com>
2023-08-31 20:23:34 -0500

    sols for 03 challenge

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(toReverse) {
+  let inReverse = toReverse.split("");
+  inReverse = inReverse.reverse();
+  let postReverse = inReverse.join("");
+  return postReverse;
 };
 
 // Do not edit below this line
