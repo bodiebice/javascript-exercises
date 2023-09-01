d8a2eb81f1e78b1dd3fd8a4fd162ee3a1058334b
bodiebice <bodiebice@gmail.com>
2023-08-31 22:14:38 -0500

    solutions for excercise 4, passes all test below estimated time.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(givenArray) {
+  const spotToRemove = null;
+  for  (y = 0; y<arguments.length; y++){
+    for (x = 0; x<givenArray.length; x++){
+      if (givenArray[x] == arguments[y] && typeof(givenArray[x]) == typeof(arguments[y])){
+        givenArray.splice(x,1);
+      }
+    }
+  }
+  return givenArray;
 
 };
 
